b8c1c972cdf62140f395fb8c9f19bdf865b2c1f4
Shreeya Bhetwal <bhetwalshreeya@gmail.com>
2020-01-24 15:51:12 -0800

    Added few files and folders

---
+++ schema.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/fetcher', {useMongoClient: true});
+
+let listingSchema = mongoose.Schema({
+neighborhood: Number,
+walk_score: Number,
+transit_score: Number,
+nearby_homes: [
+    {
+        price:Number,
+        sqft: Number,
+        bedNumber: Number,
+        bathNumner: Number,
+        address: String
+
+    }
+]
+
+});
+//google maps api for the map
+//nearby homes images
+let Repo = mongoose.model('Repo', listingSchema)
+
