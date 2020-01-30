c146ceecd4cac5c7ae491fbdc62886bac324237d
Shreeya Bhetwal <bhetwalshreeya@gmail.com>
2020-01-29 20:50:04 -0800

    got see more/less neighborhood details conditional rendering working

---
+++ client/src/NeighborSummary.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NeighborSummary = ({number}) => (
+ <div>
+<h3>This is a seller's market</h3>
+<p>There are more interested buyers than homes for sale.</p>
+<h4>Neighborhood home value</h4>
+<p><label style ={{color: 'blue'}}>{number} home values</label> have risen 0.2 % over the past 12 months.</p>
+<h4>One-year prediction</h4>
+<p>Zillow predicts the home values in label {number} will rise 0.1% in the next year. </p>
+<h4>Median home comparison</h4>
+<p>This home is valued 28.5% higher than the median home in {number} </p>
+ </div>
+)
+
+export default NeighborSummary;
