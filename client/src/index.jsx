ce3dd9d27e8eead6c0a16121d32a6b27c305511f
Shreeya Bhetwal <bhetwalshreeya@gmail.com>
2020-02-08 13:32:11 -0800

    changed render dom for proxy

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './Neighborhood.jsx';
 
 
-ReactDOM.render (<App/> , document.getElementById('app'))
+ReactDOM.render (<App/> , document.getElementsByClassName('nh')[1]);
