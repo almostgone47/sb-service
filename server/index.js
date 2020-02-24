e07e597c73c8cfab42a1541c379ef5dacf108859
almostgone47 <almostgone47@hotmail.com>
2020-02-24 14:37:35 -0800

    added newrelic and k6 stress test

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('newrelic');
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3001;
 const db = require ('../database/dbPostgres.js');
 const bodyParser = require('body-parser');
 
