608fb48f6a8d5aa2479dcdbffdde93806f3a7a3e
almostgone47 <almostgone47@hotmail.com>
2020-02-24 18:35:20 -0800

    tweak to newrelic

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ import { check, sleep } from "k6";
 
 export let options = {
  stages: [
-    { duration: "30s", target: 200 },
-    { duration: "240s", target: 1000  },
-    { duration: "30s", target: 200 },
+    { duration: "30s", target: 2000 },
+    // { duration: "30s", target: 30000 },
+    // { duration: "30s", target: 3000000 },
   ]
 };
 
 export default function() {
-  let res = http.get("http://localhost:3000");
+  let res = http.get("http://localhost:3001");
   check(res, {
     "status was 200": (r) => r.status == 200,
-    "transaction time OK": (r) => r.timings.duration < 200
+    "transaction time OK": (r) => r.timings.duration < 2000
   });
-  sleep(.1);
+  sleep(.01);
 }
