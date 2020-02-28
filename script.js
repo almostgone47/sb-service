3aa06b2700b00f3e8a292d02b5b259c415a9de37
almostgone47 <almostgone47@hotmail.com>
2020-02-28 09:55:26 -0800

    changes to k6 script

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ import { check, sleep } from "k6";
 
 export let options = {
  stages: [
-    { duration: "30s", target: 2000 },
-    // { duration: "30s", target: 30000 },
-    // { duration: "30s", target: 3000000 },
+    { duration: "30s", target: 30 },
+    { duration: "30s", target: 300 },
+    { duration: "30s", target: 3000 },
   ]
 };
 
 export default function() {
-  let res = http.get("http://localhost:3001");
+  let res = http.get("http://ec2-18-219-161-251.us-east-2.compute.amazonaws.com:3001/");
   check(res, {
     "status was 200": (r) => r.status == 200,
     "transaction time OK": (r) => r.timings.duration < 2000
